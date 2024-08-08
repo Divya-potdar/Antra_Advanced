05cb8282ce6ef51e34bcf0fe19844e0c6b72396b
Divya-potdar <divyajob2@gmail.com>
2024-08-08 09:15:48 -0400

    Add files via upload

---
+++ Redux/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CountdownTimer from './Components/CountdownTimer'; 
+import './App.css'; 
+
+function App() {
+  return (
+    <div className="App">
+      <CountdownTimer />
+    </div>
+  );
+}
+
+export default App;
